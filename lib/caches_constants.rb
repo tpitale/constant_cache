59f81978ebd922d3ad3870f5102f591cf5880e04
Patrick Reagan <patrick.reagan@viget.com>
2007-07-03 13:19:48 +0000

    Refactor code per Ben's suggestions
    git-svn-id: http://svn.extendviget.com/lab/trunk/plugins/caches_constants@134 e959a6d6-1924-0410-92b3-a6fa492f4c66

--- lib/caches_constants.rb
+++ lib/caches_constants.rb
@@ -5,9 +5,10 @@ module ActiveRecord
         options = {:key => :name, :limit => 64}.merge(additional_options)
         find(:all).each do |model| 
           const = Viget::Format.to_const(model.send(options[:key].to_sym))
-          const = const[0, options[:limit]] unless const.blank?
-          set_constant = !const.blank? && !const_defined?(const)
-          const_set(const, model) if set_constant
+          unless const.blank?
+            const = const[0, options[:limit]]
+            const_set(const, model) if !const.blank? && !const_defined?(const)
+          end
         end
       end
     end
