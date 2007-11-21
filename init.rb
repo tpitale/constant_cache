1321953a082e0da3707e4bad604e2c3bd2a0307d
Patrick Reagan <patrick.reagan@viget.com>
2007-11-21 19:16:38 +0000

    Start of refactoring to handle AR lifecycle hooks
    git-svn-id: http://svn.extendviget.com/lab/trunk/plugins/caches_constants@452 e959a6d6-1924-0410-92b3-a6fa492f4c66

--- init.rb
+++ init.rb
@@ -5,3 +5,4 @@ require "#{lib_dir}/constant_cache"
 
 String.send(:include, Viget::Format)
 ActiveRecord::Base.send(:extend, Viget::ConstantCache::ClassMethods)
+ActiveRecord::Base.send(:include, Viget::ConstantCache::InstanceMethods)
--- lib/constant_cache.rb
+++ lib/constant_cache.rb
@@ -3,17 +3,23 @@ module Viget
     module ClassMethods
       def caches_constants(additional_options = {})
         options = {:key => :name, :limit => 64}.merge(additional_options)
-        find(:all).each do |model| 
-          const = model.send(options[:key].to_sym).constant_name
-          unless const.blank?
-            const = const[0, options[:limit]]
-            if !const.blank?
-              raise RuntimeError, "Constant #{self.to_s}::#{const} has already been defined" if const_defined?(const)
-              const_set(const, model) if !const.blank?
-            end
+        find(:all).each {|model| model.set_instance_as_constant(options) }
+      end
+    end
+    
+    module InstanceMethods
+      
+      def set_instance_as_constant(options)
+        const = self.send(options[:key].to_sym).constant_name
+        unless const.blank?
+          const = const[0, options[:limit]]
+          if !const.blank?
+            raise RuntimeError, "Constant #{self.class.to_s}::#{const} has already been defined" if self.class.const_defined?(const)
+            self.class.const_set(const, self) if !const.blank?
           end
         end
       end
+      
     end
   end
 end
